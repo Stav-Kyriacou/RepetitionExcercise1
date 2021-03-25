88786fc567271fe9652298e2ae9d4d6b0cb5ff38
STAVROS KYRIACOU <101610510@student.swin.edu.au>
2021-03-25 16:13:22 +1100

    Initial project

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RepetitionExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int counter = 0;
+
+            Console.Write("How many times should I repeat? ");
+            int repeatAmount = int.Parse(Console.ReadLine());
+            
+
+            while(counter < repeatAmount){
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
+
+                Console.WriteLine(name);
+
+                counter++;
+            }
+
+            Console.WriteLine("Goodbye");
+            
+        }
+    }
+}
